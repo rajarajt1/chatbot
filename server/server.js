583f86f898e72ae62f8a4d7426b22038dbae90a1
chola <rajarajasozhan13@gmail.com>
2026-05-15 18:35:42 +0530

    fix: allow all CORS origins on backend proxy

--- server/server.js
+++ server/server.js
@@ -4,12 +4,7 @@ const cors = require('cors');
 const https = require('https');
 
 const app = express();
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://rajarajt1.github.io'
-  ]
-}));
+app.use(cors());
 app.use(express.json());
 
 // Bypass ngrok browser warning
